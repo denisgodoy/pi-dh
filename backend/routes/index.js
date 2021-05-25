5bf93a99368352e2439df38ade39e306818761ca
denisgodoy <denisgodoy1@gmail.com>
2021-05-25 15:06:29 -0300

    add validator to POST  route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const controller = require('../controllers/HomeController');
 
+const contatoValidator = require('../middlewares/ContatoValidator');
+
 router.get('/', controller.index);
-router.post('/', controller.form);
+router.post('/', contatoValidator, controller.form);
 router.get('/sucesso', controller.redirect);
 
 module.exports = router;
