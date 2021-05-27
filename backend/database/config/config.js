9bab83204a40a6a4eb8b08d7ad388f7d69ad4566
denisgodoy <denisgodoy1@gmail.com>
2021-05-27 12:36:07 -0300

    update credentials

--- backend/database/config/config.js
+++ backend/database/config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
-    "database": "database_development",
+    "password": process.env.DB_PASS,
+    "database": process.env.DATABASE,
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
