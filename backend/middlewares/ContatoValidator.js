93f516551457d4d7888b31d945f252fb610e84cf
denisgodoy <denisgodoy1@gmail.com>
2021-05-27 12:37:10 -0300

    update validator

--- backend/middlewares/ContatoValidator.js
+++ backend/middlewares/ContatoValidator.js
@@ -1,17 +1,17 @@
 const yup = require('yup');
 
-function validator(req, res, next) {
+function contatoValidator(req, res, next) {
     const schema = yup.object().shape({
         nome: yup.string().required(),
         email: yup.string().required(),
-        mensagem: yup.string().required().min(10)
+        mensagem: yup.string().required()
     });
 
     if (!schema.isValidSync(req.body)) {
         return res.status(400).json({error: 'Por favor, verifique os campos de preenchimento e tente novamente.'});
-    }
-    
+    };
+
     next(); 
 }
 
-module.exports = validator;
+module.exports = contatoValidator;
