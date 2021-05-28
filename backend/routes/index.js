7ff5a47d898fe31da689dbc4e28dfa0f1d529e5d
denisgodoy <denisgodoy1@gmail.com>
2021-05-28 10:46:50 -0300

    modify POST route to /contato

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,6 +6,6 @@ const controller = require('../controllers/HomeController');
 const contatoValidator = require('../middlewares/ContatoValidator');
 
 router.get('/', controller.index);
-router.post('/', contatoValidator, controller.sendMail);
+router.post('/contato', contatoValidator, controller.sendMail);
 
 module.exports = router;
