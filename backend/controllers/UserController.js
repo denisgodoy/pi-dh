4c895da0c2b97e341ec181bec008d5d6c18348c0
nmartineli <nataliamartineli@gmail.com>
2021-05-28 20:05:34 -0300

    Added Bcryptjs to encrypt password

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,11 +1,15 @@
 const UserService = require('../services/UserService');
+const bcryptjs = require('bcryptjs');
 
 const UserController = {
 	showSignUpPage: (req, res) => {
 		return res.render('sign-up');
 	},
 	createUser: async (req, res) => {
-		const { nome, sobrenome, email, senha, tipoUser } = req.body;
+		let { nome, sobrenome, email, senha, tipoUser } = req.body;
+
+		senha = bcryptjs.hashSync(senha, 8);
+
 		const user = await UserService.createUser(nome, sobrenome, email, senha, tipoUser);
 		return res.json(nome);
 	},
