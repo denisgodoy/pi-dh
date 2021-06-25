513e2c56a6f9a67b0d8b07e8f05571e2f8cd87bf
denisgodoy <denisgodoy1@gmail.com>
2021-06-25 14:52:16 -0300

    create method to filter by user type

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -74,5 +74,16 @@ const UserController = {
     let destroyedUser = await UserService.destroy(idUser);
     return res.json(destroyedUser);
   },
+  indexByType: async (req, res) => {
+    const { idUser } = req.params;
+    const userType = await UserService.getById(idUser);
+
+    switch (userType.tipoUser) {
+      case "aluno":
+        return res.redirect('/dashboard/aluno');
+      case "professor":
+        return res.redirect('/dashboard/professor');
+    };
+  }
 };
 module.exports = UserController;
