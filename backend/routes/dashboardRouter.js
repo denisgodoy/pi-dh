08d5ad6e06ea3e0fa977da8b0e224c9d4c00245e
denisgodoy <denisgodoy1@gmail.com>
2021-07-01 09:40:07 -0300

    update routes

--- backend/routes/dashboardRouter.js
+++ backend/routes/dashboardRouter.js
@@ -6,8 +6,7 @@ const alunoTurmaController = require('../controllers/AlunoTurmaController');
 const userController = require('../controllers/UserController');
 
 // ROTAS PROFESSOR
-router.post('/criar', alunoTurmaController.create);
-router.get('/criar/:codigo/qrcode', alunoTurmaController.createQrCode);
+
 
 //ROTAS ALUNO
 router.get('/aluno', userController.indexByType, dashboardController.indexAluno);
@@ -15,7 +14,9 @@ router.get('/aluno/turmas', alunoTurmaController.getAllTurmas);
 router.get('/aluno/turmas/:idTurma', alunoTurmaController.getTurmaById);
 router.get('/aluno/turmas/:idTurma/atividades', alunoAtividadeController.getAllAtividades);
 router.get('/aluno/turmas/:idTurma/atividades/:idAtividade', alunoAtividadeController.getAtividadeById);
-router.post('/aluno/turmas/:idTurma/atividades/:idAtividade', alunoAtividadeController.sendAtividade);
+router.post('/aluno/turmas/:idTurma/atividades/:idAtividade', alunoAtividadeController.createAssociation);
+router.post('/aluno/turmas/:idTurma/atividades/:id', alunoAtividadeController.sendAtividade);
+router.delete('/aluno/turmas/:idTurma/atividades/:id', alunoAtividadeController.destroyAssociation);
 router.get('/aluno/turmas/:idTurma/ranking');
 
 module.exports = router;
