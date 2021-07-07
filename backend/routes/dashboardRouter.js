d3e2213d0ed5361530c3e995962fdf41b9cbb7b6
denisgodoy <denisgodoy1@gmail.com>
2021-07-07 10:09:23 -0300

    test routes

--- backend/routes/dashboardRouter.js
+++ backend/routes/dashboardRouter.js
@@ -3,28 +3,28 @@ const router = express.Router();
 const dashboardController = require('../controllers/DashboardController');
 const alunoAtividadeController = require('../controllers/AlunoAtividadeController');
 const alunoTurmaController = require('../controllers/AlunoTurmaController');
-const userController = require('../controllers/UserController');
+const alunoValidator = require('../middlewares/AlunoRouteValidator');
 
 // ROTAS TESTE
 // router.post('/criar', alunoTurmaController.create);
 // router.get('/criar/:codigo/qrcode', alunoTurmaController.createQrCode);
 // router.post('/atividade', alunoAtividadeController.create);
-// router.post('/:idUser/:idAtividade', alunoAtividadeController.createAssociation);
+router.post('/:idUser/:idTurma', alunoTurmaController.createAssociation);
 // router.get('/atividade/:idAtividade', alunoAtividadeController.getAtividadeById);
 // router.post('/:id', alunoAtividadeController.sendAtividade);
-// router.get('/:idUser', alunoAtividadeController.getAllAtividades);
+router.get('/:idUser', alunoAtividadeController.getAllAtividades);
 // router.delete('/excluir/:id', alunoAtividadeController.destroyAssociation);
 
 //ROTAS ALUNO
-router.get('/', userController.indexByType);
+router.get('/', alunoValidator);
 router.get('/aluno/:idUser', dashboardController.indexAluno);
-router.get('/aluno/:idUser/turmas', alunoTurmaController.getAllClasses);
-router.get('/aluno/:idUser/turmas/:idTurma', alunoTurmaController.getClassById);
-router.get('/aluno/:idUser/turmas/:idTurma/atividades', alunoAtividadeController.getAllAtividades);
-router.get('/aluno/:idUser/turmas/:idTurma/atividades/:idAtividade', alunoAtividadeController.getAtividadeById);
-router.post('/aluno/:idUser/turmas/:idTurma/atividades/:idAtividade', alunoAtividadeController.createAssociation);
-router.post('/aluno/:idUser/turmas/:idTurma/atividades/:id', alunoAtividadeController.sendAtividade);
-router.delete('/aluno/:idUser/turmas/:idTurma/atividades/:id', alunoAtividadeController.destroyAssociation);
-router.get('/aluno/:idUser/turmas/:idTurma/ranking');
+router.get('/aluno/turmas', alunoTurmaController.getAllClasses);
+router.get('/aluno/turmas/:idTurma', alunoTurmaController.getClassById);
+router.get('/aluno/turmas/:idTurma/atividades', alunoAtividadeController.getAllAtividades);
+router.get('/aluno/turmas/:idTurma/atividades/:idAtividade', alunoAtividadeController.getAtividadeById);
+router.post('/aluno/turmas/:idTurma/atividades/:idAtividade', alunoAtividadeController.createAssociation);
+router.post('/aluno/turmas/:idTurma/atividades/:id', alunoAtividadeController.sendAtividade);
+router.delete('/aluno/turmas/:idTurma/atividades/:id', alunoAtividadeController.destroyAssociation);
+router.get('/aluno/turmas/:idTurma/ranking');
 
 module.exports = router;
