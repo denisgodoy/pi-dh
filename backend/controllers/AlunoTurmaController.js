845f773bef7928cf7123fe2cdff627586b7de3ea
denisgodoy <denisgodoy1@gmail.com>
2021-07-07 23:51:00 -0300

    update methods on controller

--- backend/controllers/AlunoTurmaController.js
+++ backend/controllers/AlunoTurmaController.js
@@ -1,23 +1,44 @@
 const AlunoTurmaService = require("../services/AlunoTurmaService");
+const UserService = require('../services/UserService');
 
 const AlunoTurmaController = {
     index: (req, res) => {
-        return res.render('turmas');
+        return res.render('aluno-turmas-dashboard');
     },
     getAllClasses: async (req, res) => {
         const { idUser } = req.params;
-        const classes = await AlunoTurmaService.getClasses(idUser);
-        return res.send(classes);
+        const student = await UserService.getById(idUser);
+        const data = await AlunoTurmaService.getClasses(idUser);
+
+        return res.render('aluno-turmas-dashboard', 
+        { 
+            student,
+            data 
+        });
     },
     getClassById: async (req, res) => {
-        const { idTurma } = req.params;
-        const classById = await AlunoTurmaService.getClassById(idTurma);
-        return res.send(classById);
+        const { idUser, idTurma } = req.params;
+        const student = await UserService.getById(idUser);
+        const data = await AlunoTurmaService.getClassById(idTurma);
+
+        return res.render('aluno-turma-dashboard', 
+        { 
+            student,
+            data 
+        });
     },
     createAssociation: async (req, res) => {
-        const { idUser, idTurma } = req.params;
-        const association = await AlunoTurmaService.createAssociation(idUser, idTurma);
-        return res.json(association)
+        const { idUser } = req.params;
+        const { idTurma } = req.body;
+        const student = await UserService.getById(idUser);
+        await AlunoTurmaService.createAssociation(idUser, idTurma);
+        const data = await AlunoTurmaService.getClasses(idUser);
+
+        return res.render('aluno-turmas-dashboard', 
+        {
+            student,
+            data
+        });
     },
     destroyAssociation: async (req, res) => {
         const { id } = req.params;
