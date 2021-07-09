ff159e84dc3b581463d656f44af95b1d7fb1d6aa
denisgodoy <denisgodoy1@gmail.com>
2021-07-09 00:01:41 -0300

    update student-activity service

--- backend/services/AlunoAtividadeService.js
+++ backend/services/AlunoAtividadeService.js
@@ -1,6 +1,26 @@
 const database = require('../database/models/index');
 
 const AlunoAtividadeService = {
+  getSubscribed: async (idUser) => {
+    const subscribed = await database.AtividadeAluno.findAll({
+      where: { idUser },
+      include: [{ model: database.Atividade }]
+    });
+    return subscribed;
+  },
+  getActivities: async (idTurma) => {
+    const activities = await database.AtividadeTurma.findAll({
+      where: { idTurma },
+      include: [{ model: database.Atividade }]
+    });
+    return activities;
+  },
+  getActivityById: async (id) => {
+    const activityById = await database.AtividadeAluno.findByPk(id, {
+      include: [{ model: database.Atividade }]
+    });
+    return activityById;
+  },
   getSentActivity: async (id) => {
     const activityByPk = await database.AtividadeAluno.findByPk(id, {
       attributes: ['textField']
@@ -14,8 +34,6 @@ const AlunoAtividadeService = {
     {
       where: { id },
     });
-    const sentActivity = await this.getSentActivity(id);
-    return sentActivity;
   },
   createAssociation: async (idUser, idAtividade) => {
     const associateToActivity = await database.AtividadeAluno.create({
