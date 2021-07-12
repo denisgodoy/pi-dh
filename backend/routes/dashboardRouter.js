2aef0bd6c87ba1e7179ccca2d06c8f9021e35796
denisgodoy <denisgodoy1@gmail.com>
2021-07-12 17:29:16 -0300

    student dashboard routes ok

--- backend/routes/dashboardRouter.js
+++ backend/routes/dashboardRouter.js
@@ -12,25 +12,17 @@ const ProfessorValidator = require('../middlewares/ProfessorRouteValidator');
 
 //ROTAS ALUNO
 router.get('/aluno', dashboardController.indexStudent);
-router.get('/aluno/profile', AlunoValidator, UserController.showUserProfile); 
+router.get('/aluno/profile', UserController.showUserProfile); 
 router.get('/aluno/turmas', alunoTurmaController.getAllClasses);
 router.post('/aluno/turmas', alunoTurmaController.createAssociation);
 router.get('/aluno/turmas/:idTurma', alunoTurmaController.getClassById);
+router.post('/aluno/turmas/:idTurma', alunoTurmaController.destroyAssociation);
 router.get('/aluno/turmas/:idTurma/atividades', alunoAtividadeController.getAllActivities);
+router.get('/aluno/turmas/:idTurma/atividades/:idAtividade', alunoAtividadeController.enrollActivity);
+router.post('/aluno/turmas/:idTurma/atividades/:idAtividade', alunoAtividadeController.createAssociation);
 router.get('/aluno/turmas/:idTurma/atividade/:id', alunoAtividadeController.getActivityById);
 router.post('/aluno/turmas/:idTurma/atividade/:id', alunoAtividadeController.sendActivity);
-
-//to-do
-// router.get('/');
-// router.delete('/aluno/turmas/:idTurma/atividade/:id', alunoAtividadeController.destroyAssociation);
-// router.post('/aluno/turmas/:idTurma', alunoTurmaController.destroyAssociation);
-// router.get('/aluno/turmas/:idTurma/atividades/:idAtividade', alunoAtividadeController.getAtividadeById);
-// router.post('/aluno/turmas/:idTurma/atividades/:idAtividade', alunoAtividadeController.createAssociation);
-// router.delete('/aluno/turmas/:idTurma/atividades/:id', alunoAtividadeController.destroyAssociation);
-// router.get('/aluno/turmas/:idTurma/ranking', rankingController.index);
-
-
-//Rota perfil de aluno
+router.post('/aluno/turmas/:idTurma/atividade/:id/desistir', alunoAtividadeController.destroyAssociation);
 
 //Rota perfil de professor
 router.get(
