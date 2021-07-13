ccd146c7b8cf60e6ba50346612c39801bbcd10cb
denisgodoy <denisgodoy1@gmail.com>
2021-07-13 11:20:44 -0300

    add popup confirmation style/javascript

--- backend/public/js/dashboard-aluno/dashboard.js
+++ backend/public/js/dashboard-aluno/dashboard.js
@@ -3,7 +3,23 @@ function hideBillboard() {
     billboard.style.display = "none";
 };
 
+function popUpConfirm(event) {
+    const leave = document.getElementById("leave-class");
+
+    if (event.target.value == "stay") {
+        leave.style.display = "none";
+    } else {
+        leave.style.display = "flex";
+    };
+};
+
 window.onload = function () {
     const closeBtn = document.getElementById("close-billboard");
     closeBtn.addEventListener("click", hideBillboard);
+
+    const stayBtn = document.getElementById("stay");
+    stayBtn.addEventListener("click", popUpConfirm);
+
+    const leaveBtn = document.getElementById("leave");
+    leaveBtn.addEventListener("click", popUpConfirm);
 };
