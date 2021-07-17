84806cd6a2d0fc22c1bfe2ff355abcaeba183888
denisgodoy <denisgodoy1@gmail.com>
2021-07-17 14:51:55 -0300

    implement middleware

--- backend/routes/dashboardRouter.js
+++ backend/routes/dashboardRouter.js
@@ -11,22 +11,23 @@ const AlunoValidator = require('../middlewares/AlunoRouteValidator');
 const ProfessorValidator = require('../middlewares/ProfessorRouteValidator');
 
 //ROTAS ALUNO
-router.get('/aluno', dashboardController.indexStudent);
-router.get('/aluno/profile', UserController.showUserProfile); 
-router.get('/aluno/turmas', alunoTurmaController.getAllClasses);
-router.post('/aluno/turmas', alunoTurmaController.createAssociation);
-router.get('/aluno/turmas/:idTurma', alunoTurmaController.getClassById);
-router.post('/aluno/turmas/:idTurma', alunoTurmaController.destroyAssociation);
-router.get('/aluno/turmas/:idTurma/ranking', rankingController.index);
-router.get('/aluno/turmas/:idTurma/atividades', alunoAtividadeController.getAllActivities);
-router.get('/aluno/turmas/:idTurma/atividades/:idAtividade', alunoAtividadeController.enrollActivity);
-router.post('/aluno/turmas/:idTurma/atividades/:idAtividade', alunoAtividadeController.createAssociation);
-router.get('/aluno/turmas/:idTurma/atividade/:id', alunoAtividadeController.getActivityById);
-router.post('/aluno/turmas/:idTurma/atividade/:id', alunoAtividadeController.sendActivity);
-router.post('/aluno/turmas/:idTurma/atividade/:id/desistir', alunoAtividadeController.destroyAssociation);
-router.get('/aluno/atividades-pendentes', alunoAtividadeController.getPending);
-router.get('/aluno/atividades-corrigidas', alunoAtividadeController.getSent);
-router.get('/aluno/novas-atividades', alunoAtividadeController.getAllNew);
+router.get('/', AlunoValidator, dashboardController.redirect);
+router.get('/aluno', AlunoValidator, dashboardController.indexStudent);
+router.get('/aluno/profile', AlunoValidator, UserController.showUserProfile); 
+router.get('/aluno/turmas', AlunoValidator, alunoTurmaController.getAllClasses);
+router.post('/aluno/turmas', AlunoValidator, alunoTurmaController.createAssociation);
+router.get('/aluno/turmas/:idTurma', AlunoValidator, alunoTurmaController.getClassById);
+router.post('/aluno/turmas/:idTurma', AlunoValidator, alunoTurmaController.destroyAssociation);
+router.get('/aluno/turmas/:idTurma/ranking', AlunoValidator, rankingController.index);
+router.get('/aluno/turmas/:idTurma/atividades', AlunoValidator, alunoAtividadeController.getAllActivities);
+router.get('/aluno/turmas/:idTurma/atividades/:idAtividade', AlunoValidator, alunoAtividadeController.enrollActivity);
+router.post('/aluno/turmas/:idTurma/atividades/:idAtividade', AlunoValidator, alunoAtividadeController.createAssociation);
+router.get('/aluno/turmas/:idTurma/atividade/:id', AlunoValidator, alunoAtividadeController.getActivityById);
+router.post('/aluno/turmas/:idTurma/atividade/:id', AlunoValidator, alunoAtividadeController.sendActivity);
+router.post('/aluno/turmas/:idTurma/atividade/:id/desistir', AlunoValidator, alunoAtividadeController.destroyAssociation);
+router.get('/aluno/atividades-pendentes', AlunoValidator, alunoAtividadeController.getPending);
+router.get('/aluno/atividades-corrigidas', AlunoValidator, alunoAtividadeController.getSent);
+router.get('/aluno/novas-atividades', AlunoValidator, alunoAtividadeController.getAllNew);
 
 //Rota perfil de professor
 router.get(
